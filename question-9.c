b469932e3cadccab05bb45eae213a894cf46d2bc
shrijan-adhikari <adhikari.shrijan03@gmail.com>
2025-09-15 16:52:01 +0530

    code done

---
+++ question-9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time;
+    float simple_interest, compound_interest, amount = 1.0;
+
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter rate of interest (in %%): ");
+    scanf("%f", &rate);
+
+    printf("Enter time (in years): ");
+    scanf("%f", &time);
+
+    // Simple Interest
+    simple_interest = (principal * rate * time) / 100;
+
+    // Compound Interest (manual power calculation)
+    for(int i = 0; i < (int)time; i++) {
+        amount *= (1 + rate / 100);
+    }
+    compound_interest = principal * amount - principal;
+
+    printf("Simple Interest = %.2f\n", simple_interest);
+    printf("Compound Interest = %.2f\n", compound_interest);
+
+    return 0;
+}
