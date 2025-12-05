2feb6ad405b5d3a5650c78256efd916f2d98c7de
shrijan-adhikari <adhikari.shrijan03@gmail.com>
2025-12-05 16:27:23 +0530

    Update question119.c

--- question119.c
+++ question119.c
@@ -1,23 +1,4 @@
-//Q119: Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
 
-/*
-Sample Test Cases:
-Input 1:
-nums1 = [1,3,3,4]
-Output 1:
-3
-
-Input 2:
-nums1 = [1,2,2]
-Output 2:
-2
-
-Input 3:
-nums1 = [0,4,1,1,5]
-Output 3:
-1
-
-*/
 #include <stdio.h>
 int findDuplicate(int* nums, int numsSize) {
     int seen[100000] = {0}; 
